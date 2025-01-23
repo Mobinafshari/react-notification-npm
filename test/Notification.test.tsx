81e186ea227b1e5b73b6ac7546ae59cab4e5e1b6
mobin afshari <mma137813781378@gmail.com>
2025-01-23 12:01:37 +0330

    feat: vitest and test dependencies added

---
+++ test/Notification.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import Notification from "../src/components/Notification";
+import userEvent from "@testing-library/user-event";
+
+describe("notification", () => {
+  it("should have button for trigger notification", () => {
+    render(<Notification />);
+    const trigger = screen.getByRole("button");
+    expect(trigger).toBeInTheDocument();
+  });
+  it("should not have toast", () => {
+    render(<Notification />);
+    const notification = screen.queryByRole("alert");
+    expect(notification).not.toBeInTheDocument();
+  });
+  it("trigger toast", async () => {
+    render(<Notification />);
+    const trigger = screen.getByRole("button");
+    const user = userEvent.setup();
+    await user.click(trigger);
+    expect(screen.getByRole("alert")).toBeInTheDocument();
+  });
+});
